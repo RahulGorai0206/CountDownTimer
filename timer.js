593267f6b672860896483f44c963f1488676c8d5
RahulGorai0202 <rahulgorai0206@gmail.com>
2023-06-30 23:40:56 +0530

    Added User Input :)

--- timer.js
+++ timer.js
@@ -1,18 +1,28 @@
-var endDate="30 Jun 2023 07:26 PM";
-const inputs = document.querySelectorAll("input");
-function clock(){
-    const end = new Date(endDate);
+function clock(end){
     const now = new Date();
     if(end>now){
+    console.log(now)
         const diff = (end - now)/1000;
-        inputs[3].value=Math.floor(diff%60);
-        inputs[2].value=Math.floor(diff/60%60);
-        inputs[1].value=Math.floor(diff/60/60);
-        inputs[0].value=Math.floor(diff/60/60/24);
+        res3.value=Math.floor(diff%60);
+        res2.value=Math.floor(diff/60%60);
+        res1.value=Math.floor(diff/60/60);
+        res0.value=Math.floor(diff/60/60/24);
     }
     else{
+        document.getElementById("txtt").style.color="yellow"
         document.getElementById("txtt").innerText="Your time is up !"
     }
 }
-clock();
-setInterval(()=>{clock()},1000)
+function startTimer(){
+    const end = new Date();
+    const hour=document.querySelector("#hour").value
+    const min=document.querySelector("#min").value
+    const sec=document.querySelector("#sec").value
+    end.setHours(end.getHours() + Number(hour));
+    end.setMinutes(end.getMinutes() + Number(min));
+    end.setSeconds(end.getSeconds() + Number(sec));
+    document.getElementById("divInp").style.display="none";
+    document.getElementById("main").style.display="flex";
+    clock(end);
+    setInterval(()=>{clock(end)},1000);
+};
